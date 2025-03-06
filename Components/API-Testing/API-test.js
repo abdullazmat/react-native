401aff59af77f0217f5d0eebfbc6bf7378fffe3f
Abdullah <shinsukenakamurahard@gmail.com>
2025-03-06 05:39:57 +0500

    Form Data Post with API

--- Components/API-Testing/API-test.js
+++ Components/API-Testing/API-test.js
@@ -10,62 +10,70 @@ import {
 } from 'react-native';
 
 function App() {
-  const [data, setData] = useState([]);
-  const getAPIData = async () => {
-    //Call API
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [email, setEmail] = useState('');
+
+  const saveAPIData = async () => {
     const url = 'http://10.0.2.2:3000/users';
-    let result = await fetch(url);
+    const formdata = {
+      name,
+      age,
+      email,
+    };
+    let result = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formdata),
+    });
     result = await result.json();
-    if (result) setData(result);
     console.log(result);
+    console.warn('Data Added');
+    setName('');
+    setEmail('');
+    setAge(0);
   };
-
-  useEffect(() => {
-    getAPIData();
-  }, []);
-
   return (
-    <ScrollView>
-      {data.length > 0 ? (
-        <FlatList
-          data={data}
-          renderItem={({item}) => (
-            <View
-              key={item?.id}
-              style={{
-                padding: 10,
-                borderColor: 'black',
-                borderWidth: 2,
-                margin: 10,
-              }}>
-              <Text
-                style={{
-                  fontSize: 20,
-                }}>
-                Id : {item?.id}
-              </Text>
-              <Text
-                style={{
-                  fontSize: 14,
-                  paddingTop: 5,
-                }}>
-                Title : {item?.name}
-              </Text>
-              <Text
-                style={{
-                  fontSize: 14,
-                  paddingTop: 5,
-                }}>
-                Body : {item?.email}
-              </Text>
-            </View>
-          )}
-        />
-      ) : null}
-    </ScrollView>
+    <View>
+      <Text style={styles.heading}>Form For Students</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        placeholder="Enter your name"
+        onChangeText={text => setName(text)}
+      />
+      <TextInput
+        style={styles.input}
+        value={email}
+        placeholder="Enter your email"
+        onChangeText={text => setEmail(text)}
+      />
+      <TextInput
+        style={styles.input}
+        value={age}
+        placeholder="Enter your age"
+        onChangeText={text => setAge(text)}
+      />
+
+      <Button title="SAVE DATA" onPress={saveAPIData} />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 20,
+    color: 'red',
+    margin: 20,
+    textAlign: 'center',
+  },
+  input: {
+    borderColor: 'orange',
+    borderWidth: 2,
+    margin: 20,
+  },
+});
 
 export default App;
